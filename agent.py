dd3a9aeaee4670a0ea4d4fa9c8c968e69996e461
automated parrot 🦜 <31025149+reevee-codes@users.noreply.github.com>
2026-01-18 17:03:40 +0100

    added agentState class

--- agent.py
+++ agent.py
@@ -1,50 +1,54 @@
+from dataclasses import dataclass
 from decision.llm_decision import llm_decide
 from environment import check_api
 
+@dataclass
+class AgentState:
+    steps: int = 0
+    ok_count: int = 0
+    error_count: int = 0
+    timeout_count: int = 0
 
 class Agent:
     def __init__(self, goal):
         self.goal = goal
-        self.steps = 0
-        self.ok_count = 0
-        self.error_count = 0
-        self.timeout_count = 0
+        self.state = AgentState()
 
     def observe(self):
-        self.steps += 1
+        self.state.steps += 1
         status = check_api()
         print(f"[OBSERVE] API status: {status}")
         if status == "OK":
-            self.ok_count += 1
+            self.state.ok_count += 1
         elif status == "ERROR":
-            self.error_count += 1
+            self.state.error_count += 1
         elif status == "TIMEOUT":
-            self.timeout_count += 1
+            self.state.timeout_count += 1
         return status
 
     def decide(self, observation):
-        state = {"ok_count": self.ok_count,
-                 "error_count": self.error_count,
-                 "timeout_count": self.timeout_count,
+        state_of_api = {"ok_count": self.state.ok_count,
+                 "error_count": self.state.error_count,
+                 "timeout_count": self.state.timeout_count,
                  "last_observation": observation}
         try:
-            llm_return =  llm_decide(state)
+            llm_return =  llm_decide(state_of_api)
             return llm_return["decision"]
         except Exception:
             pass
-        if self.ok_count == 3:
+        if self.state.ok_count == 3:
             return "STOP"
-        if self.error_count == 3:
+        if self.state.error_count == 3:
             return "STOP"
         else:
             return "CONTINUE"
 
     def act(self, decision):
         print(
-            f"Step {self.steps}, "
-            f"ok: {self.ok_count}, "
-            f"errors: {self.error_count}, "
-            f"timeouts: {self.timeout_count}, "
+            f"Step {self.state.steps}, "
+            f"ok: {self.state.ok_count}, "
+            f"errors: {self.state.error_count}, "
+            f"timeouts: {self.state.timeout_count}, "
             f"decision: {decision}"
         )
         if decision == "STOP":
